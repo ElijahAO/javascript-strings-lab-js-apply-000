48d52ced164cba266f11773b72802c30c8179f8b
Elijah Adebiyi Oyeniyi <biyibama@gmail.com>
2018-12-25 01:48:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + "Neil deGrasse Tyson" + "!";
 
 var topic = "Let's talk about your proposal";
 
-var conversation = `${"Let's talk about your proposal"}`;
+var conversation = `${"Let's talk about Let's talk about your proposal"}`;
